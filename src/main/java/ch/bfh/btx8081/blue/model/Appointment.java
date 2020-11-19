2bc7684afd3665e40949c4e1cbfa095c848d65d5
LukasBFH <loosl1@bfh.ch>
2020-11-19 17:56:53 +0100

    first version of Appointment.java, done small changes to Calendar.java

--- src/main/java/ch/bfh/btx8081/blue/model/Appointment.java
+++ src/main/java/ch/bfh/btx8081/blue/model/Appointment.java
@@ -1,10 +1,40 @@
 package ch.bfh.btx8081.blue.model;
 
+import java.time.LocalDateTime;
+
 /**
  * @author loosl1
  * <p>
  * created on 19/11/2020
  */
 public class Appointment {
-    public int appointmentID;
+
+    protected int appointmentID;
+
+    protected LocalDateTime start;
+
+    protected LocalDateTime end;
+
+    protected String title;
+
+    protected String info;
+
+    public void setShortDescription() {
+        //toDo
+    }
+
+    protected enum type {
+        TYPE1,
+        TYPE2,
+        TYPE3
+    }
+
+/* toDo
+    public void setColor(enum typeColor){
+        if (color==)
+    }
+*/
+
+
+
 }
--- src/main/java/ch/bfh/btx8081/blue/model/Calendar.java
+++ src/main/java/ch/bfh/btx8081/blue/model/Calendar.java
@@ -2,6 +2,12 @@ package ch.bfh.btx8081.blue.model;
 
 import java.util.ArrayList;
 
+/**
+ * @author loosl1
+ * <p>
+ * created on 19/11/2020
+ */
+
 public class Calendar {
 
     private ArrayList<Appointment> appointments = new ArrayList<>();
