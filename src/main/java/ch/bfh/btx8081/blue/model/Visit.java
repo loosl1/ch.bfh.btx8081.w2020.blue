62489c9174dde7da8b5051fc172ec0baf5cbf3ce
LukasBFH <loosl1@bfh.ch>
2020-11-21 17:40:20 +0100

    finished all methods except getters and setters in Visit.java

--- src/main/java/ch/bfh/btx8081/blue/model/Visit.java
+++ src/main/java/ch/bfh/btx8081/blue/model/Visit.java
@@ -22,16 +22,27 @@ public class Visit extends Appointment {
         this.treatedPatients = treatedPatients;
     }
 
+    /**
+     * saves the report given s into the constructed object
+     * @param s
+     */
     private void saveReport(String s) {
 
         this.report = s;
 
     }
 
-    //seq. diagram, loop through patients, comment
+    /**
+     *  loops through the treatedPatients List on which it searches for given Id
+     *  which was given the constructed Visit
+     *  If found, it ads the report with the id to the checklist
+     *
+     *
+     * @param visitID
+     */
     private void updateReports(int visitID) {
 
-        for (Patient patient : treatedPatients) {
+        for (Patient ignored : treatedPatients) {
 
             if (this.appointmentID == visitID) {
 
@@ -43,11 +54,16 @@ public class Visit extends Appointment {
 
     //-------------Checklist----------------------
 
+    /**
+     *
+     * @return checklist object
+     */
     public Checklist getChecklist() {
         return checklist;
     }
 
     public void setChecklist(Checklist checklist) {
+
         this.checklist = checklist;
     }
 
