847b43a4eaacf0d5a8581c6e903d1c365cbc5750
loosl1 <lukas.loosli@students.bfh.ch>
2020-12-09 15:27:34 +0100

    starrted with the VisitPresenter.java

--- src/main/java/ch/bfh/btx8081/blue/presenter/VisitPresenter.java
+++ src/main/java/ch/bfh/btx8081/blue/presenter/VisitPresenter.java
@@ -1,5 +1,9 @@
 package ch.bfh.btx8081.blue.presenter;
 
+import ch.bfh.btx8081.blue.model.Appointment;
+import ch.bfh.btx8081.blue.model.Checklist;
+import ch.bfh.btx8081.blue.model.HealthVisitor;
+import ch.bfh.btx8081.blue.model.Patient;
 import ch.bfh.btx8081.blue.view.CalendarView;
 import ch.bfh.btx8081.blue.view.VisitView;
 
@@ -10,4 +14,30 @@ import ch.bfh.btx8081.blue.view.VisitView;
  */
 public class VisitPresenter {
     private VisitView viewComponent;
+    private Patient patient;
+    private Checklist checklist;
+    private HealthVisitor currentUser;
+    private Appointment currentAppointment;
+
+    /**
+     * Constructor
+     */
+    public VisitPresenter(VisitView viewComponent){
+        this.viewComponent = viewComponent;
+
+        // manual created data
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
